7e1b600a59ed8b087051c1b46cd8ed53688d9aae
bernardo <araujo.bernardo@gmail.com>
2021-01-10 11:36:53 +0000

    inicial

---
+++ test.py
@@ -0,0 +1,13 @@
+from mmdet3d.apis import init_detector, inference_detector
+
+config_file = 'configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_kitti-3d-car.py'
+checkpoint_file = 'checkpoints/hv_pointpillars_secfpn_6x8_160e_kitti-3d-car_20200620_230614-77663cd6.pth'
+
+# build the model from a config file and a checkpoint file
+model = init_detector(config_file, checkpoint_file, device='cuda:0')
+
+# test a single image and show the results
+point_cloud = 'test.bin'
+result, data = inference_detector(model, point_cloud)
+# visualize the results and save the results in 'results' folder
+model.show_results(data, result, out_dir='results')
